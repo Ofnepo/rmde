b852ed2eaadcf39d47e1381b6c6839e9f5161e36
Raven Balsiger <raven.balsiger@gmail.com>
2023-12-28 17:30:06 +0100

    Ported to egui(yet again)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::{fs::{File, OpenOptions}, io::{Read, Write}};
 
-use fltk::{window::MenuWindow, app::App, prelude::*, text::{TextEditor, TextBuffer}, button::Button};
+use eframe::egui;
+
 
 struct RayFile{
     path: String,
     origin: String,
-    buf: TextBuffer,
-    edit: TextEditor
 }
 
 impl Default for RayFile {
@@ -14,8 +13,6 @@ impl Default for RayFile {
         Self {
             path: String::new(),
             origin: String::new(),
-            buf: TextBuffer::default(),
-            edit: TextEditor::default()
         }
     }
 }
@@ -28,15 +25,9 @@ impl RayFile {
                 let mut origin = String::new();
                 let mut file =  File::open(&path).ok().unwrap();
                 file.read_to_string(&mut origin).ok();
-                let mut buf = TextBuffer::default();
-                buf.set_text(&origin);
-                let mut edit = TextEditor::default();
-                edit.set_buffer(buf);
                 Self {
                     path: path,
                     origin: origin,
-                    buf: edit.buffer().unwrap(),
-                    edit: edit
                 }
 
             }
@@ -44,29 +35,62 @@ impl RayFile {
     }
 }
 
-fn main() {
-    let app = App::default();
-    let mut win = MenuWindow::default().with_size(1270, 720);
-    let mut file = RayFile::new("README.md".into());
-    file.edit.set_size(win.width()-160, win.height()-40);
-    file.edit.set_pos(150, 30);
-    let mut save_bt = Button::new(2, 2, 50, 26, "Save");
+fn main() -> Result<(), eframe::Error> {
+    //let mut file = RayFile::new("README.md".into());
 
-    save_bt.set_callback(move| c |{
-        let mut save_file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(false)
-            .open(&file.path)
-            .unwrap();
-        file.origin = file.buf.text();
-        save_file.write_all(file.origin.as_bytes()).expect("did't work idk");
-        c.damage();
-    });
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_| {
+            // This gives us image support:
 
-    
-    win.end();
-    win.show();
+            Box::<MyApp>::default()
+        }),
+    )
+}
 
-    app.run().unwrap();
+
+struct MyApp {
+    current: RayFile,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            current: RayFile::default()
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui|{
+                if ui.button("Save").clicked() {
+                    let mut save_file = OpenOptions::new()
+                        .write(true)
+                        .create(true)
+                        .truncate(false)
+                        .open(&self.current.path)
+                        .unwrap();
+                    save_file.write_all(&self.current.origin.as_bytes()).ok();
+                }
+                if ui.button("Open").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_file() {
+                        self.current = RayFile::new(path.display().to_string());
+                    }
+                }
+            });
+            ui.horizontal(|ui| {
+                ui.text_edit_multiline(&mut self.current.origin);
+            });
+            
+
+            
+        });
+    }
 }
